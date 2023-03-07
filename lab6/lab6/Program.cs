fcb401ea8448550570adaa597ed09e4235df621e
rhythmxqq <96514857+rhythmxqq@users.noreply.github.com>
2023-03-07 09:45:37 +0300

    123
    123

---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+public class Animal
+{
+    public string location { get; set; }
+    public string food { get; set; }
+    public Animal(string food, string location)
+    {
+        this.food = food;
+        this.location = location;
+    }
+
+    public virtual void makeNoise()
+    {
+        Console.WriteLine("Животное спит");
+    }
+
+    public virtual void eat()
+    {
+        Console.WriteLine("Животное кушает");
+    }
+
+    public void sleep()
+    {
+        Console.WriteLine("Животное спит");
+    }
+}
+
+public class Dog : Animal
+{
+    private string breed;
+
+    public Dog(string food, string location, string breed) : base(food, location)
+    {
+        this.breed = breed;
+    }
+
+    public override void makeNoise()
+    {
+        Console.WriteLine("Собака лает");
+    }
+
+    public override void eat()
+    {
+        Console.WriteLine("Собака кушает кости");
+    }
+}
+
+public class Cat : Animal
+{
+    private bool hasClaws;
+
+    public Cat(string food, string location, bool hasClaws) : base(food, location)
+    {
+        this.hasClaws = hasClaws;
+    }
+
+    public override void makeNoise()
+    {
+        Console.WriteLine("Кошка мурлычет");
+    }
+
+    public override void eat()
+    {
+        Console.WriteLine("Кошка кушает рыбу");
+    }
+}
+
+public class Horse : Animal
+{
+    private int speed;
+
+    public Horse(string food, string location, int speed) : base(food, location)
+    {
+        this.speed = speed;
+    }
+
+    public override void makeNoise()
+    {
+        Console.WriteLine("Лошадь ржет");
+    }
+
+    public override void eat()
+    {
+        Console.WriteLine("Лошадь кушает сено");
+    }
+}
+
+public class Veterinarian
+{
+    public void treatAnimal(Animal animal)
+    {
+        Console.WriteLine($"Животное ест {animal.food} и находится в {animal.location}");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Dog dog = new Dog("кости", "дворе", "овчарка");
+        Cat cat = new Cat("рыба", "квартире", true);
+        Horse horse = new Horse("сено", "стойле", 20);
+
+        Veterinarian vet = new Veterinarian();
+        vet.treatAnimal(dog);
+        vet.treatAnimal(cat);
+        vet.treatAnimal(horse);
+
+        dog.makeNoise();
+        cat.makeNoise();
+        horse.makeNoise();
+
+        dog.eat();
+        cat.eat();
+        horse.eat();
+    }
+}
